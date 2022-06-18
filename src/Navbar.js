41a83cea60228272bf756c712792031143e27957
nnhoa11 <88938984+nnhoa11@users.noreply.github.com>
2022-06-18 23:28:04 +0700

    patch 1

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,16 +10,16 @@ class Header extends Component {
         const path = this.props.location.pathname.slice(1);
         return ( 
             < nav className = "navbar" >
-            <img className = "logo" alt = "Skitus Logo"src = { logo } >
-            </img> 
-            <h1 className = "part" alt = "web-part" id = "web-part" > { path } </h1> 
-            <div className = "links" >
-                <Link to = "/" > Trang chủ </Link> 
-                <Link to = "/Trend" > Xu hướng </Link> 
-                <Link to = "/Course" > Khóa học </Link> 
-                <Link to = "/Team" > Đội ngũ </Link> 
-            </div> 
-            <div class="searchBox">
+                <img className = "logo" alt = "Skitus Logo"src = { logo } >
+                </img> 
+                <h1 className = "part" alt = "web-part" id = "web-part" > { path } </h1> 
+                <div className = "links" >
+                    <Link to = "/" > Trang chủ </Link> 
+                    <Link to = "/Trend" > Xu hướng </Link> 
+                    <Link to = "/Course" > Khóa học </Link> 
+                    <Link to = "/Team" > Đội ngũ </Link> 
+                </div> 
+                <div class="searchBox">
                     <input class="searchInput"type="text" name="" placeholder="Search"/>
                     <button class="searchButton" href="#">
                         <i class="material-icons">
@@ -27,6 +27,8 @@ class Header extends Component {
                         </i>
                     </button>
                 </div>
+                <button className="accBtn">Đăng Kí</button>
+                <button className="accBtn">Đăng Nhập</button>
             </nav>
         );
     }
