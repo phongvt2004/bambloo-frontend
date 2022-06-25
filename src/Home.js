6ae4098f66f370003ac12db12a61c8ac31f6a61a
nnhoa11 <minecraftcuaxi@gmail.com>
2022-06-26 00:41:25 +0700

    patch 2

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 import background1 from "./resource/background1.png"
+import background2 from "./resource/background2.png"
+import background3 from "./resource/background3.png"
+import background4 from "./resource/background4.png"
 import logo from "./resource/logo.png"
 import './Home.css'
+const second = 1000
 function Home(){
+    document.getElementById('p2').src = background1;
+    setInterval(() => {
+        console.log('background changed')
+        const urlList = [background1,background2,background3,background4]  
+        const bg = urlList[Math.floor(Math.random() * urlList.length)]
+        document.getElementById('p2').src = bg;
+    },5 * second)
     return(
         <div className="Home">
             <div className="homecontent">
@@ -18,25 +29,29 @@ function Home(){
                     <span id="footContent">Được tin dùng bởi trên 15.000 học sinh sinh viên cả nước</span>
                 </div>
                 <div id="right">
-                    <div className="box">
-                        <div class="boxIcon">
-                            <i>NOTHING</i>
-                            <p>Number</p>
-                            <p>title</p>
+                    <div className="box" id="box1">
+                        <div class="boxContent">
+                            <div class="icon" id="i1"></div>
+                            <p class="pnum">Number</p>
+                            <p class="pchar">title</p>
                         </div>
                     </div>
-                    <div className="box">
-                        <div class="boxIcon">
-                            <i class="material-icons">person</i>
-                            <p>Number</p>
-                            <p>title</p>
+                    <div className="box" id="box2">
+                        <div class="boxContent" >
+                            <div class="icon" id="i2">
+                                <i class="material-icons">near_me</i>
+                            </div>
+                            <p class="pnum">Number</p>
+                            <p class="pchar">title</p>
                         </div>
                     </div>
-                    <div className="box">
-                        <div class="boxIcon">
-                            <i class="material-icons">near_me</i>
-                            <p>Number</p>
-                            <p>title</p>
+                    <div className="box" id="box3">
+                        <div class="boxContent">
+                            <div class="icon" id="i3">                                
+                                <i class="material-icons">person</i>
+                            </div>
+                            <p class="pnum">Number</p>
+                            <p class="pchar">title</p>
                         </div>
                     </div>
                   
@@ -46,7 +61,7 @@ function Home(){
                 <div id="p1">
                     {/* <img src={background1}alt="img" /> */}
                 </div>
-                <img id="p2" src={background1}alt="img" />
+                <img id="p2"  alt="img" />
             </div>
         </div>
     );
