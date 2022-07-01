7028f9ad3ff246190a4aeafd8e70824080ca831f
nnhoa11 <minecraftcuaxi@gmail.com>
2022-07-01 16:53:03 +0700

    patch 2.2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import Trend from './Trend';
 import Course from './Course'
 import Team from './Team'
+import background1 from './resource/background1.png'
 // import {logo} from './resource/logo.png'
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
@@ -10,27 +11,25 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Router>
-      <div>
-        <NavBar />
-        <div className="content" style={{"width":"100%"}}>
-          <Switch>
-              <Route exact path="/"> 
-                <Home/>
-              </Route>
-              <Route path="/Trend"> 
-                <Trend/>
-              </Route>
-              <Route path="/Course"> 
-                <Course/>
-              </Route>
-              <Route path="/Team"> 
-                <Team/>
-              </Route>
-          </Switch>
-        </div>
-      </div>
-      </Router>
+        <Router>
+            <NavBar />
+            <div className="content" style={{"width":"100%"}}>
+              <Switch>
+                  <Route exact path="/"> 
+                    <Home/>
+                  </Route>
+                  <Route path="/Trend"> 
+                    <Trend/>
+                  </Route>
+                  <Route path="/Course"> 
+                    <Course/>
+                  </Route>
+                  <Route path="/Team"> 
+                    <Team/>
+                  </Route>
+              </Switch>
+            </div>
+        </Router>
     </div> 
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
-import background1 from "./resource/background1.png"
+// import background1 from "./resource/background1.png"
+import background1 from "./resource/test.jpg"
 import background2 from "./resource/background2.png"
 import background3 from "./resource/background3.png"
 import background4 from "./resource/background4.png"
@@ -6,17 +7,26 @@ import logo from "./resource/logo.png"
 import './Home.css'
 const second = 1000
 function Home(){
-    document.getElementById('p2').src = background1;
+    const backgroundImg = [background1, background2, background3, background4]
+    const buttonclr = ["linear-gradient(278.76deg, #017AF1 10.43%, #01F4FE 106.11%)",
+                    "linear-gradient(278.76deg, #30A010 10.43%, #81D364 106.11%)",
+                    "linear-gradient(278.76deg, #398491 10.43%, #78B8C9 106.11%)",
+                    "linear-gradient(278.76deg, #EFBB6A 10.43%, #FFF9D8 106.11%);"]
+    let i = 0
     setInterval(() => {
-        console.log('background changed')
-        const urlList = [background1,background2,background3,background4]  
-        const bg = urlList[Math.floor(Math.random() * urlList.length)]
-        document.getElementById('p2').src = bg;
-    },5 * second)
+        document.getElementById("BackgroundImg").src = backgroundImg[Math.trunc(i)]
+        document.getElementById("btn1").style.background     = buttonclr[Math.trunc(i)]
+        if (i < 3) {
+            i++
+        } else {
+            i = 0
+        }
+        console.log(i+"")
+      }, 5*second);
     return(
         <div className="Home">
             <div className="homecontent">
-                <div className="left">
+                `<div className="left">
                     <div class  ="topContent">
                         <h3>HỌC TRỰC TUYẾN</h3>
                         <h1>Giáo dục nền tảng số</h1>
@@ -58,10 +68,7 @@ function Home(){
                 </div>
             </div>
             <div className="background">
-                <div id="p1">
-                    {/* <img src={background1}alt="img" /> */}
-                </div>
-                <img id="p2"  alt="img" />
+                <img src={background1} id="BackgroundImg" alt="img" />
             </div>
         </div>
     );
